28de604bb4bd7b3d2fd9fcff665ee238de657842
fengbozhi <P>
2018-07-24 18:17:40 +0800

    'create'

---
+++ examples/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/introduce',
+      name: 'introduce',
+      component: r => require.ensure([], () => r(require('../docs/introduce.md')))
+    }
+  ]
+})
